d47aaad7b9d7bc65d8af747c335250688f803fa8
Andreas Karlsson <andreas@jhkarlsson.com>
2017-02-10 15:16:24 +0100

    Add smart IUnknown/COM pointer

---
+++ src/ComPtr.h
@@ -0,0 +1,92 @@
+#pragma once
+
+namespace Unmanagedpp
+{
+	// Wraps a IUknown (COM) interface pointer
+	// in a ref class to automatically handle AddRef/Release calls.
+	template <typename T>
+	public ref class ComPtr
+	{
+	private:
+		// Store a T** instead of a T* so clients can pass
+		// a T** to factory methods.
+		T** ppIUnknown; 
+		ComPtr(const ComPtr<T>% rhs);
+	public:
+		ComPtr(T* iunknown) : ppIUnknown(new T*)
+		{
+			*ppIUnknown = iunknown;
+			(*ppIUnknown)->AddRef();
+		}
+
+		ComPtr() : ppIUnknown(new T*)
+		{
+			*ppIUnknown = nullptr;
+		}
+
+		~ComPtr()
+		{
+			(*this).!ComPtr();
+		}
+
+		!ComPtr()
+		{
+			Reset();
+			delete ppIUnknown;
+		}
+
+		void Reset()
+		{
+			Reset(nullptr);
+		}
+
+		// Set the interface pointer.
+		void Reset(T* iunknown)
+		{
+			// Release old
+			if (*ppIUnknown)
+				(*ppIUnknown)->Release();
+			// Replace
+			*ppIUnknown = iunknown;
+
+			// AddRef new
+			if (*ppIUnknown)
+				(*ppIUnknown)->AddRef();
+		}
+
+		// Many / most COM interfaces are created by passing a 
+		// pointer to an interface pointer. This method allows access to such a ptr
+		T** Ref()
+		{
+			return ppIUnknown;
+		}
+
+		T* Get()
+		{
+			return *ppIUnknown;
+		}
+
+		T* operator-> ()
+		{
+			return *ppIUnknown;
+		}
+
+		ComPtr<T>% operator=(T* rhs)
+		{
+			Reset(rhs);
+			return *this;
+		}
+
+		bool operator==(T* rhs)
+		{
+			return *ppIUnknown == rhs;
+		}
+
+		explicit operator bool()
+		{
+			return *ppIUnknown != nullptr;
+		}
+	};
+
+
+}
---
+++ test/TestComPtr.cpp
@@ -0,0 +1,127 @@
+
+#include <ComPtr.h>
+#include <Windows.h>
+#include "Helpers.h"
+using namespace System;
+using namespace System::Text;
+using namespace System::Collections::Generic;
+using namespace Microsoft::VisualStudio::TestTools::UnitTesting;
+
+using namespace Unmanagedpp;
+
+namespace Test
+{
+
+	class TestComObject : public IUnknown
+	{
+	private:
+		ULONG refCount = 0;
+	public:
+		STDMETHODIMP QueryInterface(REFIID riid, void **ppv)
+		{
+			return E_ABORT; // N/A
+		}
+		STDMETHODIMP_(ULONG) AddRef()
+		{
+			return ++refCount;
+		}
+		STDMETHODIMP_(ULONG) Release()
+		{
+			ULONG newRefCount = --refCount;
+			if (newRefCount == 0)
+				delete this;
+			return newRefCount;
+		}
+
+		int RefCount()
+		{
+			return refCount;
+		}
+
+		int OneLess(int n)
+		{
+			return n - 1;
+		}
+	};
+
+
+	[TestClass]
+	public ref class ComPtrTests
+	{
+	public:
+		[TestMethod]
+		void TestReference()
+		{
+			TestComObject *a = new TestComObject();
+			ComPtr<TestComObject> b(a);
+			Assert::IsTrue(a == b.Get());
+		}
+
+		[TestMethod]
+		void TestArrow()
+		{
+			ComPtr<TestComObject> b(new TestComObject());
+			Assert::AreEqual(b->OneLess(10), 9);
+		}
+
+		[TestMethod]
+		void TestRefCount()
+		{
+			TestComObject *a = new TestComObject();
+			{
+				ComPtr<TestComObject> b(a);
+				Assert::AreEqual(a->RefCount(), 1);
+
+				a->AddRef();
+				b = nullptr;
+				Assert::AreEqual(a->RefCount(), 1);
+
+				b = a;
+				Assert::AreEqual(a->RefCount(), 2);
+			}
+			Assert::AreEqual(a->RefCount(), 1);
+		}
+
+		[TestMethod]
+		void TestOutPtr()
+		{
+			ComPtr<TestComObject> b;
+
+			*b.Ref() = new TestComObject();
+
+			Assert::AreEqual(b->OneLess(4), 3);
+		}
+
+		[TestMethod]
+		void TestEmpty()
+		{
+			ComPtr<TestComObject> b;
+
+			Assert::IsTrue(b.Get() == nullptr);
+		}
+
+		[TestMethod]
+		void TestEquals()
+		{
+			auto a = new TestComObject();
+			ComPtr<TestComObject> b(a);
+
+			Assert::IsTrue(b == a);
+		}
+
+		[TestMethod]
+		void TestBool()
+		{
+			auto a = new TestComObject();
+			ComPtr<TestComObject> b;
+
+			Assert::IsFalse((bool)b);
+
+			b = a;
+
+			Assert::IsTrue((bool)b);			
+		}
+
+	};
+
+}
